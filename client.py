f3f7817a7b81f881f9176b5b2b3164b2507ee75b
Tudor Gorea <73231149+GoreaTudor@users.noreply.github.com>
2023-04-04 19:21:24 +0300

    hello phase done

--- client.py
+++ client.py
@@ -1,28 +1,45 @@
 import socket
+import json
 import time
 
-messages = ["client hello", "client data", "client bye", "RST"]
 
-if __name__ == '__main__':
-    host = socket.gethostname()  # both apps running on same pc
-    port = 5000
-    client_socket = socket.socket()  # create new socket
+cipher_specs = ["RSA_AES"]
+
+
+def send_receive(message):
+    client_socket.send(message.encode())
+    print("\nSent:", str(message))
     
-    client_socket.connect((host, port))  # connect to the server
-    print("Connected to server")
+    data = client_socket.recv(2048).decode()
+    print("Received:", str(data))
     
-    index = 0
+    return json.loads(data)
+# end send_receive
+
+
+# takes the data from the server as parameter and returns the next message to send
+def SSL_HandShake():
+    # Hello phase: cipher suites
+    h1 = '{"id": "H1", "text": "client hello", "cipher_suites": ["RSA_AES"]}'
+    h2 = send_receive(h1)
     
-    while index < len(messages):
-        message = messages[index]
-        
-        client_socket.send(message.encode())  # send message
-        print("\nSent:", str(message))
-        
-        data = client_socket.recv(1024).decode()  # wait for server to send message
-        print("Received:", str(data))
-        
-        time.sleep(0.5)  # sleeps in seconds
-        index += 1  # prepare next message
+    if h2["id"] != "H2":
+        print("Expected H2 header")
+        return
     
-    client_socket.close()  # close the connection
+    chosen_suite = h2["cipher_suite"]
+# end SSL_HandShake
+
+
+# client script:
+host = socket.gethostname()  # both apps running on same pc
+port = 5000
+client_socket = socket.socket()  # create new socket
+
+client_socket.connect((host, port))  # connect to the server
+print("Connected to server")
+
+SSL_HandShake()
+
+client_socket.close()  # close the connection
+
--- server.py
+++ server.py
@@ -1,35 +1,55 @@
 import socket
+import json
 import time
 
-messages = ["server hello", "server data", "server bye", "RST ACK"]
 
-if __name__ == '__main__':
-    host = socket.gethostname()  # both apps running on same pc
-    port = 5000
-    server_socket = socket.socket()  # create new socket
-    server_socket.bind((host, port))  # binds host address and port together
+cipher_specs = ["RSA_AES"]
+
+
+def receive():
+    data = conn.recv(2048).decode()
+    print("\nReceived:", str(data))
     
-    server_socket.listen(1)  # config how many clients the server can listen to
-    print("Server started")
+    if not data:
+        conn.close()
+        quit()
     
-    conn, adr = server_socket.accept()  # accept new connection to client
-    print("Connection accepted, address is:", str(adr))
-    
-    index = 0
-    
+    return json.loads(data)
+# end receive
+
+def send(message):
+    conn.send(message.encode())
+    print("Sent:", str(message))
+# end send
+
+
+def SSL_HandShake():
     while True:
-        data = conn.recv(1024).decode()  # receive data from client
-        print("\nReceived:", str(data))
+        h1 = receive()
         
-        # process message and prepare answer...
-        if not data:
-            break  # if data is not received, then break
+        if h1["id"] != "H1":
+            print("Expected H2 header")
+            return
         
-        message = messages[index]  # prepare message
-        time.sleep(0.5)
-        index += 1
+        chosen_suite = 'RSA_AES'
+        h2 = '{"id": "H2", "text": "server hello", "cipher_suite": "' + chosen_suite + '"}'
+        send(h2)
+# end SSL_HandShake
 
-        conn.send(message.encode())  # send message to client
-        print("Sent:", str(message))
-    
-    conn.close()
+
+# server script:
+host = socket.gethostname()  # both apps running on same pc
+port = 5000
+server_socket = socket.socket()  # create new socket
+server_socket.bind((host, port))  # binds host address and port together
+
+server_socket.listen(1)  # config how many clients the server can listen to
+print("Server started")
+
+conn, adr = server_socket.accept()  # accept new connection to client
+print("Connection accepted, address is:", str(adr))
+
+SSL_HandShake()
+
+conn.close()
+
